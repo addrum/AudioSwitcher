04dd4b14483d4b2e40b26ee674ade559c913a65e
addrum <adamshort1994@gmail.com>
2015-08-14 13:50:48 +0100

    Added setup project and packacged .Net451 with it.

--- SwitchAudioDevices/Properties/Settings.Designer.cs
+++ SwitchAudioDevices/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace SwitchAudioDevices.Properties {
                 this["Hotkey"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool Startup {
+            get {
+                return ((bool)(this["Startup"]));
+            }
+            set {
+                this["Startup"] = value;
+            }
+        }
     }
 }
